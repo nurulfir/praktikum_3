af854f10996f6921593e8ad280ca74c074169b25
nurulfir <muhammadnurulfirdaus05@gmail.com>
2023-11-01 08:21:12 +0700

    praktikum3

---
+++ latihan2.py
@@ -0,0 +1,11 @@
+n=input("masukkan nilai n:")
+t=input("masukkan nilai t:")
+print("Variable n=",n)
+print("Variable t=",t)
+print("Hasil penggabungan {0}&{1}={2}".format(n, t, n+t))
+
+# konversi nilai variable
+n=int(n)
+t=int(t)
+print("Hasil penjumlahan {0}+{1}={2}".format(n, t, n+t))
+print("Hasil pembagian {0}/{1}={2:.2f}".format(n, t, n/t))
---
+++ latihan3.py
@@ -0,0 +1,11 @@
+print("Hello ini script pyhon")
+
+print("    *".format("*" *1))
+print("   ***".format("*" *2))
+print("  *****".format("*" *5))
+print(" *******".format("*" *7))
+print("*********".format("*" *9))
+print(" *******".format("*" *7))
+print("  *****".format("*" *5))
+print("   ***".format("*" *3))
+print("    *".format("*" *1))
